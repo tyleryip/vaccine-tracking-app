2223293f8e63e88d03bdc41da867c1d997481e04
Tyler Yip <tyler.yip1@ucalgary.ca>
2021-03-17 21:42:58 -0600

    Feat: Fixed all the models in the tracker app, made migrations, and applied them to the database

--- cpsc471-project/vaccinetrackingapp/tracker/admin.py
+++ cpsc471-project/vaccinetrackingapp/tracker/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Import all (*) from tracker.models
 from .models import *
+
+# Registering all models under the admin user (so the admin user can edit them from the admin site)
--- cpsc471-project/vaccinetrackingapp/tracker/models.py
+++ cpsc471-project/vaccinetrackingapp/tracker/models.py
@@ -1,9 +1,14 @@
 import datetime
 
 from django.db import models
+from django.db.models import constraints
 from django.utils import timezone
 
 # Create your models here.
+# Note: Django does not support compound primary keys, so for entities we had
+# multiple primary keys as part of a compound key, we create surrogate keys
+# and enforced the compound primary key constraint with unique_together metadata
+
 class Vaccine(models.Model):
     DIN_no = models.IntegerField(primary_key=True)
     disease_treated = models.CharField(max_length=200)
@@ -15,8 +20,15 @@ class Vaccine(models.Model):
         return "DIN: " + str(self.DIN_no) + " - " + self.disease_treated
 
 class VaccineSideEffect(models.Model):
-    DIN_no = models.ForeignKey(Vaccine, on_delete=models.CASCADE, primary_key=True)
-    side_effect_name = models.CharField(max_length=200, primary_key=True)
+    vaccine_side_effect_id = models.IntegerField(primary_key=True)
+    vaccine_DIN_no = models.OneToOneField(Vaccine, on_delete=models.CASCADE)
+    side_effect_name = models.CharField(max_length=200)
+
+    # This internal meta class provides meta data to the model
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['vaccine_DIN_no', 'side_effect_name'], name='VaccineSideEffect PK')
+        ]
 
     def __str__(self):
         return "DIN: " + str(self.DIN_no) + " - " +  self.side_effect_name
@@ -31,11 +43,17 @@ class VaccinationSite(models.Model):
         return "Address: " + self.address
 
 class StoredAt(models.Model):
-    DIN_no = models.ForeignKey(Vaccine, on_delete=models.CASCADE, primary_key=True)
-    address = models.ForeignKey(VaccinationSite, on_delete=models.CASCADE, primary_key=True)
+    stored_at_id= models.IntegerField(primary_key=True)
+    DIN_no = models.OneToOneField(Vaccine, on_delete=models.CASCADE)
+    vaccination_site_address = models.OneToOneField(VaccinationSite, on_delete=models.CASCADE)
     temperature = models.FloatField(default=20)
     humidity = models.FloatField(default=60)
     lighting = models.FloatField(default=0)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['DIN_no', 'vaccination_site_address'], name='StoredAt PK')
+        ]
 
     def __str__(self):
         return "DIN: " + str(self.din_no) + " - " + self.site_address
@@ -47,13 +65,20 @@ class DisposalSite(models.Model):
 
     def __str__(self):
         return "Address: " + self.address
+
 class DisposedAt(models.Model):
-    DIN_no = models.ForeignKey(Vaccine, on_delete=models.CASCADE, primary_key=True)
-    address = models.ForeignKey(DisposalSite, on_delete=models.CASCADE, primary_key=True)
+    disposed_at_id = models.IntegerField(primary_key=True)
+    DIN_no = models.OneToOneField(Vaccine, on_delete=models.CASCADE)
+    disposal_site_address = models.OneToOneField(DisposalSite, on_delete=models.CASCADE)
     sharp = models.BooleanField(default=True)
     biohazard_leakage = models.BooleanField(default=True)
 
-    def _str__(self):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['DIN_no', 'disposal_site_address'], name='DisposedAt PK')
+        ]
+
+    def __str__(self):
         return "Din: " + str(self.DIN_no) + " - Address: " + str(self.site_address)
 
 class Doctor(models.Model): 
@@ -70,20 +95,20 @@ class Doctor(models.Model):
         return "Doctor HCC: " + str(self.hcc_no) + " - " + str(self.firstName) + " " + str(self.lastName)
 
 class Civilian(models.Model):
-    hcc_no = models.IntegerField(max_length=9, primary_key=True)
+    hcc_no = models.IntegerField(primary_key=True)
     phone_no = models.IntegerField()
     sex = models.CharField(max_length=1)
     address = models.CharField(max_length=200)
     age = models.IntegerField()
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    doctor_hcc = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    doctor_hcc = models.OneToOneField(Doctor, on_delete=models.CASCADE)
 
     def __str__(self):
         return "Civilian HCC: " + str(self.hcc_no) + " - " + self.firstName + " " + self.lastName
 
 class RiskFactor(models.Model):
-    hcc_no = models.ForeignKey(Civilian, on_delete=models.CASCADE, primary_key=True)
+    hcc_no = models.OneToOneField(Civilian, on_delete=models.CASCADE, primary_key=True)
     location = models.CharField(max_length=200)
     occupation = models.CharField(max_length=200)
     at_risk_age = models.BooleanField()
@@ -92,8 +117,9 @@ class RiskFactor(models.Model):
         return "HCC: " + str(self.hcc_no)
 
 class HealthCondition(models.Model):
-    hcc_no = models.ForeignKey(Civilian, on_delete=models.CASCADE, primary_key=True)
-    condition = models.CharField(max_length=200, primary_key=True)
+    health_condition_id = models.IntegerField(primary_key=True)
+    hcc_no = models.OneToOneField(Civilian, on_delete=models.CASCADE)
+    condition = models.CharField(max_length=200)
 
     def __str__(self):
         return "HCC: " + str(self.hcc_no) + " - " + self.condition
@@ -106,7 +132,7 @@ class Nurse(models.Model):
     age = models.IntegerField()
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
-    site_address = models.ForeignKey(VaccinationSite, on_delete=models.CASCADE)
+    site_address = models.OneToOneField(VaccinationSite, on_delete=models.CASCADE)
 
     def __str__(self):
         return "Nurse HCC: " + str(self.hcc_no) + " - " + self.firstName + " " + self.lastName
@@ -114,14 +140,15 @@ class Nurse(models.Model):
 class Appointment(models.Model):
     appointment_id = models.IntegerField(primary_key=True)
     time = models.DateTimeField()
-    vaccine_DIN_no = models.ForeignKey(Vaccine, on_delete=models.CASCADE)
-    nurse_hcc_no = models.ForeignKey(Nurse, on_delete=models.CASCADE)
-    civilian_hcc_no = models.ForeignKey(Civilian, on_delete=models.CASCADE)
-    site_address = models.ForeignKey(Vaccine, on_delete=models.CASCADE)
+    vaccine_DIN_no = models.OneToOneField(Vaccine, on_delete=models.CASCADE)
+    nurse_hcc_no = models.OneToOneField(Nurse, on_delete=models.CASCADE)
+    civilian_hcc_no = models.OneToOneField(Civilian, on_delete=models.CASCADE)
+    vaccination_site_address = models.OneToOneField(VaccinationSite, on_delete=models.CASCADE)
 
 class DoctorCertification(models.Model):
-    doctor_hcc_no = models.ForeignKey(Doctor, on_delete=models.CASCADE, primary_key=True)
-    certification = models.CharField(max_length=200, primary_key=True)
+    doctor_certification_id = models.IntegerField(primary_key=True)
+    doctor_hcc_no = models.OneToOneField(Doctor, on_delete=models.CASCADE)
+    certification = models.CharField(max_length=200)
 
     def __str__(self):
         return "Doctor HCC: " + str(self.doctor_hcc_no) + " - " + self.certification
@@ -136,10 +163,10 @@ class PpeSupplier(models.Model):
         return "Supplier Name: " + str(self.name) + " - contact info: " + str(self.contact_phone)
 
 class Ppe(models.Model):
-    ppe_id = models.IntegerField(max_length=9, primary_key=True)
+    ppe_id = models.IntegerField(primary_key=True)
     is_disposable = models.BooleanField()
-    supplier_name = models.ForeignKey(PpeSupplier, on_delete=models.CASCADE)
-    nurse_hcc = models.ForeignKey(Nurse, on_delete=models.CASCADE)
+    supplier_name = models.OneToOneField(PpeSupplier, on_delete=models.CASCADE)
+    nurse_hcc = models.OneToOneField(Nurse, on_delete=models.CASCADE)
 
     def __str__(self):
         return "PPE ID: " + str(self.ppe_id) + " - Manufactured by: " + str(self.supplier_name) + " - Used by: " + str(self.nurse_hcc)
