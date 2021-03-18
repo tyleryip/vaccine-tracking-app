7f453c3b26eb51c511500c805c2767ae9789d4bd
Tyler Yip <tyler.yip1@ucalgary.ca>
2021-03-17 22:02:43 -0600

    Feat: Added the tracker models to the track/admin.py file so admins can edit these database tables, added templates folder for later

--- cpsc471-project/vaccinetrackingapp/tracker/admin.py
+++ cpsc471-project/vaccinetrackingapp/tracker/admin.py
@@ -3,6 +3,21 @@ from django.contrib import admin
 # Register your models here.
 
 # Import all (*) from tracker.models
-from .models import *
+from .models import Vaccine, VaccineSideEffect, VaccinationSite, StoredAt, DisposalSite, DisposedAt, Doctor, Civilian, RiskFactor, HealthCondition, Nurse, Appointment, DoctorCertification, PpeSupplier, Ppe
 
 # Registering all models under the admin user (so the admin user can edit them from the admin site)
+admin.site.register(Vaccine)
+admin.site.register(VaccineSideEffect)
+admin.site.register(VaccinationSite)
+admin.site.register(StoredAt)
+admin.site.register(DisposalSite)
+admin.site.register(DisposedAt)
+admin.site.register(Doctor)
+admin.site.register(Civilian)
+admin.site.register(RiskFactor)
+admin.site.register(HealthCondition)
+admin.site.register(Nurse)
+admin.site.register(Appointment)
+admin.site.register(DoctorCertification)
+admin.site.register(PpeSupplier)
+admin.site.register(Ppe)
