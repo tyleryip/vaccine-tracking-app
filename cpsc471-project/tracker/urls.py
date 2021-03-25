d45b7925913e1f3ac19dd93ebed4cc26ca1b4703
Tyler Yip <tyler.yip1@ucalgary.ca>
2021-03-25 14:51:09 -0600

    ### Feat: Added more paths for nurses and civilians

--- cpsc471-project/tracker/urls.py
+++ cpsc471-project/tracker/urls.py
@@ -3,7 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # This page is the root, you will be presented with two buttons to be able to select which user type you will have
     path('', views.index, name='index'),
-    path('nurse_login/', views.nurse_login, name='nurse_login'),
-    path('civilian_login/', views.civilian_login, name='civilian_login')
+
+    # These pages are for the civilian and nurse login, you'll be presented with a textbox to fill in your HCC and then click a button
+    # We will also have to implement a new user section
+    path('nurse', views.nurse_login, name='nurse_login'),
+    path('civilian', views.civilian_login, name='civilian_login'),
+
+    # After logging in, civilians will be able to access their homepage
+    # Ex. civilian/1/
+    path('civilian/<int:hcc_no>/', views.civilian_homepage, name='civilian_homepage'),
+
+    path('nurse/<int:hcc_no>/', views.nurse_homepage, name='nurse_homepage')
 ]
--- cpsc471-project/tracker/views.py
+++ cpsc471-project/tracker/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import render # Required for the views page to make use of the render() method
 from .models import * # Import all models from the tracker app to use in the views
 
@@ -6,19 +7,29 @@ from django.http import HttpResponse
 
 # Main page to pick which user type you are, also display information about the website
 def index(request):
-    return HttpResponse("This is the tracker index.")
+    return HttpResponse("This is the index page, we will implement the login selection page here. (Choose civilian or nurse and get sent to the login pages)")
 
 # Login screen for nurses
 def nurse_login(request):
-    return HttpResponse("This is the login site for the nurse.")
+    return HttpResponse("This is the login site for the nurse. (You will be able to enter your nurse HCC and press login)")
 
 # Login screen for civilian
 def civilian_login(request):
-    return HttpResponse("This is the login site for the civilian.")
+    return HttpResponse("This is the login site for the civilian. (You will be able to enter your civilian HCC and press login)")
 
-# Civilian Views:
+# Civilian Views: #########################################################
 def civilian_homepage(request, hcc_no):
-    
-    return HttpResponse("This is your information %s")
+    try:
+        my_civilian = Civilian.objects.get(hcc_no = hcc_no)
+    except Civilian.DoesNotExist:
+        raise Http404("Civilian does not exist")
+    return HttpResponse("This is your information %s" % my_civilian)
 
-# Nurse Views:
+
+# Nurse Views: #########################################################
+def nurse_homepage(request, hcc_no):
+    try:
+        my_nurse = Nurse.objects.get(hcc_no = hcc_no)
+    except Nurse.DoesNotExist:
+        raise Http404("Nurse does not exist")
+    return HttpResponse("This is your information %s" % my_nurse)
