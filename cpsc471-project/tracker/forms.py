7f3c372dacb308d362851b19e85e79915bff8b20
Trevor Brown <trevorscottbrown@gmail.com>
2021-04-02 13:49:31 -0600

    Can now effectively inject new civilians into the database
    It works now - todo: error checking should inputs not be strings / ints.

--- cpsc471-project/tracker/forms.py
+++ cpsc471-project/tracker/forms.py
@@ -6,25 +6,3 @@ from django import forms
 class hcc_form(forms.Form):
     hcc_no = forms.NumberInput()
 
-
-class civilian_registration_form(forms.Form):
-    hcc_no = forms.IntegerField(label = 'civ-hcc')
-    phone_no = forms.IntegerField(label = 'phone-no')
-    sex = forms.CharField('sex')
-    address = forms.CharField(label = 'address', max_length=200)
-    age = forms.IntegerField(label = 'age')
-    first_name =  forms.CharField(label = 'first-name', max_length=200)
-    last_name =  forms.CharField(label = 'last-name', max_length=200)
-    doctor_hcc = forms.IntegerField(label = 'doctor-hcc')
-
-
-class nurse_registration_form(forms.Form):
-    hcc_no = forms.IntegerField(label = 'civ-hcc')
-    phone_no = forms.IntegerField(label = 'phone-no')
-    sex = forms.CharField('sex')
-    address = forms.CharField(label = 'address', max_length=200)
-    age = forms.IntegerField(label = 'age')
-    first_name =  forms.CharField(label = 'first-name', max_length=200)
-    last_name =  forms.CharField(label = 'last-name', max_length=200)
-    site_address = forms.CharField(label = 'site-address', max_length = 200)
-
