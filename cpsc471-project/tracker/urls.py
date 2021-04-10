2a4e67de5487c704ce469629de3405a3f2710644
Tyler Yip <tyler.yip1@ucalgary.ca>
2021-04-09 21:34:15 -0600

    ### Feat: Added the nurse's vaccination site and disposal site endpoints

--- cpsc471-project/tracker/urls.py
+++ cpsc471-project/tracker/urls.py
@@ -30,6 +30,10 @@ urlpatterns = [
     #path('nurse/<int:hcc_no>/vaccines/<int:DIN_no>', views.nurse_vaccine_details, name='nurse_vaccine_details'),
     path('nurse/vaccines', views.nurse_vaccines, name='nurse_vaccines'),
     path('nurse/vaccines/<int:DIN_no>', views.nurse_vaccine_details, name='nurse_vaccine_details'),
+
+    path('nurse/disposal_sites/', views.nurse_disposal_sites, name='nurse_disposal_sites'),
+    path('nurse/vaccination_sites/', views.nurse_vaccination_sites, name='nurse_vaccination_sites'),
+
     path('nurse/<int:hcc_no>/ppe', views.nurse_ppe, name='nurse_ppe'),
     path('nurse/add/', views.new_nurse, name='new_nurse'),
     path('nurse/<int:hcc_no>/edit/', views.edit_nurse, name='edit_nurse'),
