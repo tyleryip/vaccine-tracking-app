53774548e7c6951fd3e73917a7c6f97a98beb901
Tyler Yip <tyler.yip1@ucalgary.ca>
2021-04-12 12:32:11 -0600

    ### Feat: Added SQL logging when DEBUG mode is set to true

--- cpsc471-project/vaccinetrackingapp/settings.py
+++ cpsc471-project/vaccinetrackingapp/settings.py
@@ -122,3 +122,79 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+###### LOGGING FOR SQL STATEMENTS ######
+
+import logging
+
+# Custom sql loggin adapted from: https://stackoverflow.com/questions/57386193/django-view-formatted-sql-in-console
+class SQLFormatter(logging.Formatter):
+    def format(self, record):
+
+        # Check if Pygments is available for coloring
+        try:
+            import pygments
+            from pygments.lexers import SqlLexer
+            from pygments.formatters import TerminalTrueColorFormatter
+        except ImportError:
+            pygments = None
+
+        # Check if sqlparse is available for indentation
+        try:
+            import sqlparse
+        except ImportError:
+            sqlparse = None
+
+        # Remove leading and trailing whitespaces
+        sql = record.sql.strip()
+
+        if sqlparse:
+            # Indent the SQL query
+            sql = sqlparse.format(sql, reindent=True)
+
+        if pygments:
+            # Highlight the SQL query
+            sql = pygments.highlight(
+                sql,
+                SqlLexer(),
+                #TerminalTrueColorFormatter(style='monokai')
+                TerminalTrueColorFormatter()
+            )
+
+        # Set the records statement to the formatted query
+        record.statement = sql
+        return super(SQLFormatter, self).format(record)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'sql': {
+            '()': SQLFormatter,
+            'format': '[%(duration).3f] %(statement)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+        'sql': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'sql',
+            'level': 'DEBUG',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['sql'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.db.backends.schema': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
